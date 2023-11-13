813e40d0d2c0e9e600cfea45aa981d3a11630774
OlegVitko <oleg.vitko@gmail.com>
2023-11-13 13:28:13 +0200

    simple front end change; switch back to master travisCI build branch

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Learn React NOW!
+                        Learn React NOW with Google Kubernetes Engine aka GKE!
                     </a>
                     <Link to="/">Home</Link>
                     <Link to="/otherpage">Other Page</Link>
